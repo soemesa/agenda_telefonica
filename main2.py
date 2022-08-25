e11a1780db3109618ed8e1df470ec2c7545114e2
Soilenyis <soilenyis@gmail.com>
2022-08-25 13:29:25 -0300

    Arrumado função pesquisar

--- main2.py
+++ main2.py
@@ -146,25 +146,18 @@ def listar():
     with open('agenda_telefonica.csv', 'r') as arquivo:
         arquivo = csv.reader(arquivo, delimiter=',')
 
-        num = 0
+        num = -1
+        etiquetas = ['nome', 'email', 'telefone', 'twitter', 'instagram']
+
         for linha in arquivo:
             num += 1
             # print(linha)
-            if not 'nome' or 'email' in linha:
-                print(f"{'num':^3} | {'nome':^8} | {'email':^16} | {'telefone':^8} | {'twitter':^8} | {'instagram':^8}")
-            print(f'{num:^3} | {linha[0]:^8} | {linha[1]:^16} | {linha[2]:^8} | {linha[3]:^8} | {linha[4]:^8}')
-            print(f"{'-' * 3} | {'-' * 8} | {'-' * 16} | {'-' * 8} | {'-' * 8} | {'-' * 8}")
+            if etiquetas == linha:
+                print(f"{'num':^3} | {'nome':^8} | {'email':^16} | {'telefone':^10} | {'twitter':^10} | {'instagram':^10}")
+                print(f"{'-' * 3} | {'-' * 8} | {'-' * 16} | {'-' * 10} | {'-' * 10} | {'-' * 10}")
+            else:
+                print(f'{num:^3} | {linha[0]:^8} | {linha[1]:^16} | {linha[2]:^10} | {linha[3]:^10} | {linha[4]:^10}')
 
-
-# def testando_listar():
-#     with open('agenda_telefonica.csv', 'r') as arquivo:
-#         arquivo = csv.reader(arquivo, delimiter=',')
-#
-#         print(f"{'nome':^10} | {'email':^16} | {'telefone':^12} | {'twitter':^10} | {'instagram':^10}")
-#         print(f"{'-'*10} | {'-'*16} | {'-'*12} | {'-'*10} | {'-'*10}")
-#
-#         for linha in arquivo:
-#
 
 # TODO Ver como se comporta si el nombre tiene espacios
 def alterar():
@@ -204,4 +197,4 @@ def validar_telefone(telefone):
         return False
 
 
-listar()
+principal()
